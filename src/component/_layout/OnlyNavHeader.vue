c3ff27e3b8976980c19897a1505516e7f761093e
Jen Hoang <jenhoang304@gmail.com>
2024-05-08 11:18:52 -0700

    Create Header for home page

---
+++ src/component/_layout/OnlyNavHeader.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+<template>
+  <header class="f-w-container">
+    <nav>
+      <a href="/about" class="sub-heading">About</a>
+    </nav>
+  </header>
+</template>
+<style scoped>
+nav {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 9.8rem;
+  margin: auto;
+}
+nav a {
+  color: var(--color-secondary-300);
+}
+</style>
--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -1,3 +1,7 @@
+<script setup>
+import OnlyNavHeader from '@/component/_layout/OnlyNavHeader.vue';
+</script>
 <template>
+  <OnlyNavHeader />
   <main>hello this is home view</main>
 </template>
