c7c83b650c41c72134353ee88af70e362fd51106
Jen Hoang <jenhoang304@gmail.com>
2024-05-09 09:59:54 -0700

    Make sure the search bar will in center of the page

--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -3,18 +3,30 @@ import OnlyNavHeader from '@/component/_layout/OnlyNavHeader.vue';
 import BookSearchBar from '@/component/BookSearchBar.vue';
 </script>
 <template>
-  <OnlyNavHeader />
+  <OnlyNavHeader :class="[$style['home-header']]" />
   <main class="f-w-container">
-    <div class="row">
+    <div :class="[$style['home-content'], 'row']">
       <div class="col-6 m-auto">
-        <img src="@/asset/logo/logo.svg" alt="logo" :class="['m-auto', $style['home-logo']]" />
+        <img src="@/asset/logo/logo.svg" alt="logo" :class="[$style['home-logo']]" />
         <BookSearchBar />
       </div>
     </div>
   </main>
 </template>
 <style module>
+.home-header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+.home-content {
+  height: 80vh;
+  align-items: center;
+}
 .home-logo {
   display: block;
+  margin: auto;
+  margin-bottom: 3.2rem;
 }
 </style>
