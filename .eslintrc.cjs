9534803ff9b89663ed5b6490360fbb6001707b1e
Jen Hoang <meolony99@gmail.com>
2024-06-06 14:05:08 -0700

    Add vitest globals eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,27 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   overrides: [
     {
-      files: [
-        'e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:playwright/recommended'
-      ]
-    }
+      files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:playwright/recommended'],
+    },
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/*.test.{j,t}s?(x)'],
+      extends: ['plugin:vitest-globals/recommended'],
+      env: {
+        'vitest-globals/env': true,
+      },
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+};
