fa2f9d20579bec662f97d2b847dfbb7612da79f4
Jen Hoang <meolony99@gmail.com>
2024-06-10 11:40:06 -0700

    Set up vitest configuration

---
+++ __mocks__/book-item.js
@@ -0,0 +1,77 @@
+const bookItem = {
+  kind: 'books#volume',
+  id: '_M9vDwAAQBAJ',
+  etag: 'pwC3/AtFyHE',
+  selfLink: 'https://www.googleapis.com/books/v1/volumes/_M9vDwAAQBAJ',
+  volumeInfo: {
+    title: 'Dare to Lead',
+    subtitle: 'Brave Work. Tough Conversations. Whole Hearts.',
+    authors: ['Brené Brown'],
+    publisher: 'Random House',
+    publishedDate: '2018-10-09',
+    description:
+      '#1 NEW YORK TIMES BESTSELLER • Brené Brown has taught us what it means to dare greatly, rise strong, and brave the wilderness. Now, based on new research conducted with leaders, change makers, and culture shifters, she’s showing us how to put those ideas into practice so we can step up and lead. Don’t miss the five-part HBO Max docuseries Brené Brown: Atlas of the Heart! NAMED ONE OF THE BEST BOOKS OF THE YEAR BY BLOOMBERG Leadership is not about titles, status, and wielding power. A leader is anyone who takes responsibility for recognizing the potential in people and ideas, and has the courage to develop that potential. When we dare to lead, we don’t pretend to have the right answers; we stay curious and ask the right questions. We don’t see power as finite and hoard it; we know that power becomes infinite when we share it with others. We don’t avoid difficult conversations and situations; we lean into vulnerability when it’s necessary to do good work. But daring leadership in a culture defined by scarcity, fear, and uncertainty requires skill-building around traits that are deeply and uniquely human. The irony is that we’re choosing not to invest in developing the hearts and minds of leaders at the exact same time as we’re scrambling to figure out what we have to offer that machines and AI can’t do better and faster. What can we do better? Empathy, connection, and courage, to start. Four-time #1 New York Times bestselling author Brené Brown has spent the past two decades studying the emotions and experiences that give meaning to our lives, and the past seven years working with transformative leaders and teams spanning the globe. She found that leaders in organizations ranging from small entrepreneurial startups and family-owned businesses to nonprofits, civic organizations, and Fortune 50 companies all ask the same question: How do you cultivate braver, more daring leaders, and how do you embed the value of courage in your culture? In this new book, Brown uses research, stories, and examples to answer these questions in the no-BS style that millions of readers have come to expect and love. Brown writes, “One of the most important findings of my career is that daring leadership is a collection of four skill sets that are 100 percent teachable, observable, and measurable. It’s learning and unlearning that requires brave work, tough conversations, and showing up with your whole heart. Easy? No. Because choosing courage over comfort is not always our default. Worth it? Always. We want to be brave with our lives and our work. It’s why we’re here.” Whether you’ve read Daring Greatly and Rising Strong or you’re new to Brené Brown’s work, this book is for anyone who wants to step up and into brave leadership.',
+    industryIdentifiers: [
+      {
+        type: 'ISBN_13',
+        identifier: '9780399592522',
+      },
+      {
+        type: 'ISBN_10',
+        identifier: '0399592520',
+      },
+    ],
+    readingModes: {
+      text: false,
+      image: false,
+    },
+    pageCount: 321,
+    printType: 'BOOK',
+    categories: ['Business & Economics'],
+    maturityRating: 'NOT_MATURE',
+    allowAnonLogging: false,
+    contentVersion: '0.5.0.0.preview.0',
+    panelizationSummary: {
+      containsEpubBubbles: false,
+      containsImageBubbles: false,
+    },
+    imageLinks: {
+      smallThumbnail:
+        'http://books.google.com/books/content?id=_M9vDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+      thumbnail:
+        'http://books.google.com/books/content?id=_M9vDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+    },
+    language: 'en',
+    previewLink:
+      'http://books.google.ca/books?id=_M9vDwAAQBAJ&printsec=frontcover&dq=best+book&hl=&cd=2&source=gbs_api',
+    infoLink: 'http://books.google.ca/books?id=_M9vDwAAQBAJ&dq=best+book&hl=&source=gbs_api',
+    canonicalVolumeLink:
+      'https://books.google.com/books/about/Dare_to_Lead.html?hl=&id=_M9vDwAAQBAJ',
+  },
+  saleInfo: {
+    country: 'CA',
+    saleability: 'NOT_FOR_SALE',
+    isEbook: false,
+  },
+  accessInfo: {
+    country: 'CA',
+    viewability: 'PARTIAL',
+    embeddable: true,
+    publicDomain: false,
+    textToSpeechPermission: 'ALLOWED',
+    epub: {
+      isAvailable: false,
+    },
+    pdf: {
+      isAvailable: false,
+    },
+    webReaderLink: 'http://play.google.com/books/reader?id=_M9vDwAAQBAJ&hl=&source=gbs_api',
+    accessViewStatus: 'SAMPLE',
+    quoteSharingAllowed: false,
+  },
+  searchInfo: {
+    textSnippet:
+      'It’s why we’re here.” Whether you’ve read Daring Greatly and Rising Strong or you’re new to Brené Brown’s work, this book is for anyone who wants to step up and into brave leadership.',
+  },
+};
+export default bookItem;
--- vitest.config.js
+++ vitest.config.js
@@ -1,4 +1,4 @@
-import { fileURLToPath } from 'node:url';
+import { fileURLToPath, URL } from 'node:url';
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 import viteConfig from './vite.config';
 
@@ -10,6 +10,12 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
       environmentMatchGlobs: [['**/*.component.test.js', 'jsdom']],
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@mocks': fileURLToPath(new URL('./__mocks__', import.meta.url)),
+      },
     },
   }),
 );
